37c1c457e265b8528a5a06c53a46a44dc2f24717
MdNihal05 <112348834+MdNihal05@users.noreply.github.com>
2025-02-21 20:06:18 +0530

    Update authController.js

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -43,6 +43,7 @@ exports.login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
