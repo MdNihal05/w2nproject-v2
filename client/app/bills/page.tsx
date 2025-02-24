01a960cf87f55a68eb2a817eae3da14b4fec8e52
mdnihal05 <mdnihal01430@gmail.com>
2025-02-24 09:59:02 +0530

    fix:cookie

--- client/app/bills/page.tsx
+++ client/app/bills/page.tsx
@@ -1,8 +1,19 @@
+"use client";
 import { BillForm } from "@/components/bills/bill-form";
+import { RootState } from "@/lib/store/store";
 import React from "react";
+import { useSelector } from "react-redux";
 
-const page = () => {
-  return <BillForm />;
+const Billage = () => {
+  const { isAuthenticated } = useSelector((state: RootState) => state.auth);
+
+  return isAuthenticated ? (
+    <BillForm />
+  ) : (
+    <h1 className="text-2xl  bg-purple-500 shadow-md rounded-md p-8">
+      Your are not Authunticated, Please Login to continue...
+    </h1>
+  );
 };
 
-export default page;
+export default Billage;
--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
+"use client";
 import { BillList } from "@/components/bills/bill-list";
+import { RootState } from "@/lib/store/store";
+import { useSelector } from "react-redux";
 
 export default function DashboardPage() {
-  return (
+  const { isAuthenticated } = useSelector((state: RootState) => state.auth);
+
+  return isAuthenticated ? (
     <div className=" space-y-8 text-white min-w-screen min-h-screen">
       <h1 className="text-4xl font-bold text-center m-2 border border-purple-600 rounded-full p-2   ">
         Dashboard
@@ -10,5 +15,9 @@ export default function DashboardPage() {
         <BillList />
       </div>
     </div>
+  ) : (
+    <h1 className="text-2xl bg-purple-500 shadow-md rounded-md p-8">
+      Your are not Authunticated, Please Login to continue...
+    </h1>
   );
 }
